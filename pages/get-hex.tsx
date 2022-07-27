b0ff978cf596be8a869b55d9ea47ff71e1e24d17
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2022-07-27 09:16:57 -0400

    Much cleaner code 🧼

--- pages/get-hex.tsx
+++ pages/get-hex.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import HexInterpretations from '../data/HexInterpretations'
 import ParticleBackGround from '../components/ParticleBackGround'
 
@@ -7,67 +7,60 @@ export default function GetHex() {
     // i ching has 64 possible outcomes
     const randomValue: number = Math.floor(Math.random() * 64) + 1
 
-    const [value, setValue] = useState()
+    const [value, setValue] = React.useState<number>()
     
     const handleDivinateClick = () => {
         setValue(randomValue)
     }
 
     // this is for the share button
-    const [isCopied, setIsCopied] = useState(false);
+    const [isCopied, setIsCopied] = React.useState<boolean>(false)
 
     const handleCopyClick = () => {
-            setIsCopied(true);
-            setTimeout(() => {
-            setIsCopied(false);
-            }, 2500);
-        }
+        setIsCopied(true);
+        setTimeout(() => {
+        setIsCopied(false);
+        }, 2500);
+    }
 
   return (
     <div>
         <ParticleBackGround />
 
-        {/* output all interpretations */}
-        {/* {HexInterpretations.map((data) => (
-            <p key={data.id}>{data.id}: {data.title}, {data.description}</p>
-        ))} */}
+        <div className="flex justify-center">
+            {value ? null : <p className="font-semibold pt-5">kill the noise</p>}
+        </div>
 
-        {/* {value ? null : <p>kill the noise</p>} */}
+        <ul className=''>
+            {HexInterpretations.map((data) => (
+                <li key={data.id}>
+                        {/* UPPER & LOWER TRIGRAMS */}
+                    <p className="flex justify-center">{value === data.id ? `${data.upper} over ${data.lower}` : null}</p>
+                        {/* IMAGE */}
+                    <span className="flex justify-center">{value === data.id ? <img src={data.image} alt='Hexagram'/> : null}</span>
+                        {/* HEXAGRAM NUMBER */}
+                    <p className="flex justify-center">{value === data.id ? `Hexagram # ${value}` : null}</p>
+                        {/* TITLE */}
+                    <p className="flex justify-center">{value === data.id ? data.title : null}</p>
+                        {/* DESCRIPTION */}
+                    <div className="flex justify-center">
+                        <p className="w-2/4 text-center">{value === data.id ? data.description : null}</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
 
-        {/* gets the trigrams */}
-        {HexInterpretations.map((data) => (
-            value === data.id ? <p>{data.upper} over {data.lower}</p> : null
-                ))}
-
-        {/* gets the image */}
-        {HexInterpretations.map((data) => (
-            value === data.id ? <img src={data.image} alt='Hexagram'/> : null
-        ))}
-
-        {/* gets hexagram number */}
-        {HexInterpretations.map((data) => (
-            value === data.id ? <p>Hexagram # {value}</p> : null
-                ))}
-
-
-        {/* gets the title */}
-        {HexInterpretations.map((data) => (
-            <h2 key={data.id}>{
-                value === data.id ? data.title : null
-            }</h2>
-        ))}
 
         {value ? null :
-        <div className="flex justify-center pt-96">
-            <button
-                type="button"
-                className="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-indigo-600 bg-white hover:text-gray-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={handleDivinateClick}
-            >
-                🌘 Divinate 🌒
-            </button>
-        {/* {console.log(randomValue)} */}
-        </div>
+            <div className="flex justify-center pt-96">
+                <button
+                    type="button"
+                    className="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-indigo-600 bg-white hover:text-gray-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={handleDivinateClick}
+                >
+                    🌘 Divinate 🌒
+                </button>
+            </div>
         }
 
 
