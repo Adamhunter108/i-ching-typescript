adb922b39359fd268526aea14a70a9308bc4aac7
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2022-12-29 16:03:39 -0500

    PWA dependency, manifest and config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/////////////////////////////////
+// 👆 OG CONFIG
+/////////////////////////////////
+
+
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  // disable: process.env.NODE_ENV === "development",
+});
+
+// Use `withPWA` and pass general Next.js config
+module.exports = withPWA({    
+  reactStrictMode: true,
+  webpack5: true,
+  webpack: (config) => {
+      config.resolve.fallback = { fs: false };
+      return config;
+  },
+  output: "standalone"
+});
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,16 +7,18 @@ export default function Document() {
             <meta charSet="utf-8" />
             <link rel="icon" type="image/ico" href="/favicon.ico" />
             <meta name="robots" content="index, follow" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#000000" />
 
                 {/* OPEN GRAPH */}
             <meta property="og:site_name" content="I Ching: the Book of Changes" key="ogsitename" />
             <meta property="og:title" content="I Ching: the Book of Changes" key="og:title" />
             <meta property="og:description" content="Consult the Book of Changes with a daily I Ching reading." key="ogdesc" />
-            {/* <meta property="og:url" content="https://www.bookofchanges.app" key="ogurl" /> */}
-            {/* <meta property="og:image" content="https://www.bookofchanges.app/images/yinyang.png" key="ogimage" /> */}
+            <meta property="og:url" content="https://www.bookofchanges.app" key="ogurl" />
+            <meta property="og:image" content="https://www.bookofchanges.app/icon-192x192.png" key="ogimage" />
             <meta property="og:type" content="web-app" />
 
-            {/* <link rel="canonical" href="https://www.bookofchanges.app/" /> */}
+            <link rel="canonical" href="https://www.bookofchanges.app/" />
 
                 {/* FAVICONS  */}
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -25,11 +27,12 @@ export default function Document() {
             <link rel="manifest" href="/site.webmanifest" />
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
-            <meta name="theme-color" content="#000000" />
+
+            
 
                 {/* GOOGLE FONT CDN */}
-                <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400&display=swap" rel="stylesheet" />
 
         </Head>
         <body>
