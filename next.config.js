4927bc8e9fc55227f69f4476efec58673dd3e8e4
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-01-12 11:22:55 -0500

    PWA disabled for local dev & auth pages padding tweak

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
-  // disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
 });
 
 // Use `withPWA` and pass general Next.js config
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -61,7 +61,7 @@ export default function SignIn() {
                 </svg>
             </div>
 
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="flex min-h-full items-center justify-center pt-4 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
 
