953b269eaa00ee0931b41d99f95e222cbe8867fc
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-03-03 16:53:08 -0500

    SessionContextProvider
    and disabled PWA local dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    // Create a new supabase browser client on every first render.
+    const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+    
+    // OG next.js return
+    // return <Component {...pageProps} />
+
+    // For supabase auth
+    return (
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    )    
 }
 
 export default MyApp
